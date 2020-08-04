808ebfe664c73671d0aa27d6118a62c78445449c
jfiliceatcsumb <jfilice@csumb.edu>
2020-08-04 11:38:31 -0700

    Add files via upload
    Script to remove a directory user group (e.g. from Active Directory) from a Mac's ard_interact group. This has been tested with macOS 10.13 and 10.14.

---
+++ ard_interact_REMOVE_Lab_Remote_Access.sh
@@ -0,0 +1,30 @@
+﻿#!/bin/bash
+
+# Script to remove a directory user group (e.g. from Active Directory) from a Mac's ard_interact group.
+# This has been tested with macOS 10.13 and 10.14.
+
+##################
+# Edit variables in this section
+# 
+# User directory group to remove from ard_interact. 
+addARDgroup="MyDomain\Domain Users"
+# 
+##################
+
+# test if ard_interact group exists already
+test_ard_interact=$(/usr/sbin/dseditgroup ard_interact) 2>/dev/null
+# If $test_ard_interact string is emtpy, the group does not exist.
+if [ -z "$test_ard_interact" ] 
+then
+	echo "The ard_interact group not found. Exiting..."
+    exit
+
+else
+
+    echo "ard_interact group found. Removing $addARDgroup Users group from ard_interact group..."
+    /usr/sbin/dseditgroup -v -o edit -d "$addARDgroup" -t group ard_interact
+
+fi
+
+
+exit
