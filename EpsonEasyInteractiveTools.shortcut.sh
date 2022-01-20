4119b90bb7f9101f1d885b743f1d9664a68d8aec
jfiliceatcsumb <jfilice@csumb.edu>
2022-01-19 16:46:16 -0800

    Update EpsonEasyInteractiveTools.shortcut.sh

--- EpsonEasyInteractiveTools.shortcut.sh
+++ EpsonEasyInteractiveTools.shortcut.sh
@@ -4,21 +4,51 @@
 # jfilice@csumb.edu
 # Technology Support Services in IT
 # California State University, Monterey Bay
-# http://it.csumb.edu
+# https://csumb.edu/it
 
 
 
-# Description.
+# This script requires .
+# Run it with no arguments. 
 # 
-# Postponed script execution in DeployStudio. Commands must run as root.
+# Use as script in Jamf JSS.
+
 
 # Change History:
-# 2015/11/06:	Creation.
+# 2022/01/19:	Creation.
 #
 
-
 SCRIPTNAME=`/usr/bin/basename "$0"`
-SCRIPTPATH=`/usr/bin/dirname "$0"`
+SCRIPTDIR=`/usr/bin/dirname "$0"`
+
+# Jamf JSS Parameters 1 through 3 are predefined as mount point, computer name, and username
+
+pathToScript=$0
+mountPoint=$1
+computerName=$2
+userName=$3
+
+shift 3
+# Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
+
+echo pathToScript=$pathToScript
+echo mountPoint=$mountPoint
+echo computerName=$computerName
+echo userName=$userName
+
+# set alias for PlistBuddy and several others so I don't have to specify full path.
+# Prefix sudo path because I'm using it here for all commands.
+# If I want to run a command without the alias, then specify the full path.
+alias PlistBuddy="/usr/libexec/PlistBuddy"
+alias chown="/usr/sbin/chown"
+alias chmod="/bin/chmod"
+alias ditto="/usr/bin/ditto"
+alias defaults="/usr/bin/defaults"
+alias rm="/bin/rm"
+alias cp="/bin/cp"
+alias mkdir="/bin/mkdir"
+alias sudo=/usr/bin/sudo
+
 
 echo "***Begin $SCRIPTNAME script***"
 /bin/date
@@ -44,4 +74,6 @@ fi
 echo "***End $SCRIPTNAME script***"
 /bin/date
 
+
 exit 0
+
