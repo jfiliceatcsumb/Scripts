c9bb82b010143440ab6d8d2479204297bf710b69
jfiliceatcsumb <jfilice@csumb.edu>
2022-04-26 15:48:41 -0700

    Create Rename_Boot_Volume_to_string.sh

---
+++ Rename_Boot_Volume_to_string.sh
@@ -0,0 +1,60 @@
+﻿#!/bin/sh
+
+# Jason Filice
+# jfilice@csumb.edu
+# Technology Support Services in IT
+# California State University, Monterey Bay
+# https://csumb.edu/it
+
+
+# This script will rename the boot volume name to string input as Parameter 4. 
+# If the parameter is left blank, then the volume will not be renamed.
+# 
+# Use as script in Jamf JSS.
+
+
+# Change History:
+# 2021/03/10:	Creation.
+#
+
+SCRIPTNAME=`/usr/bin/basename "$0"`
+SCRIPTPATH=`/usr/bin/dirname "$0"`
+
+# Jamf JSS Parameters 1 through 3 are predefined as mount point, computer name, and username
+
+pathToScript=$0
+mountPoint=$1
+computerName=$2
+userName=$3
+
+shift 3
+# Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
+
+echo pathToScript=$pathToScript
+echo mountPoint=$mountPoint
+echo computerName=$computerName
+echo userName=$userName
+
+# set alias for PlistBuddy and several others so I don't have to specify full path.
+# Prefix sudo path because I'm using it here for all commands.
+# If I want to run a command without the alias, then specify the full path.
+alias PlistBuddy="/usr/libexec/PlistBuddy"
+alias chown="/usr/sbin/chown"
+alias chmod="/bin/chmod"
+alias ditto="/usr/bin/ditto"
+alias defaults="/usr/bin/defaults"
+alias rm="/bin/rm"
+alias cp="/bin/cp"
+alias mkdir="/bin/mkdir"
+alias sudo=/usr/bin/sudo
+
+
+# Example:
+# /bin/ls -FlOah "${SCRIPTPATH}"
+if [[ "$1" != "" ]]; then
+	
+	/usr/sbin/diskutil renameVolume / "${1}"
+	# Command will echo the result to stdout.
+fi
+
+exit
