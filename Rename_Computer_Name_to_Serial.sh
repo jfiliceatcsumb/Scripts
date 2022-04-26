29b3f5b4908f091d4dbccd5cb1b9b58bfa96c5be
jfiliceatcsumb <jfilice@csumb.edu>
2022-04-26 15:48:47 -0700

    Create Rename_Computer_Name_to_Serial.sh

---
+++ Rename_Computer_Name_to_Serial.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/sh
+
+# Jason Filice
+# jfilice@csumb.edu
+# Technology Support Services in IT
+# California State University, Monterey Bay
+# http://csumb.edu/it
+
+
+# This script will rename the computer name to serial number.
+
+
+
+SCRIPTNAME=`/usr/bin/basename "$0"`
+SCRIPTPATH=`/usr/bin/dirname "$0"`
+
+
+/usr/local/bin/jamf setComputerName -useSerialNumber
+
+/usr/local/bin/jamf recon
+
+
+exit 0
