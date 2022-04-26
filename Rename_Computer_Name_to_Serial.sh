6041afe3f120d272fa77ef7080a1b5383bd58ca0
jfiliceatcsumb <jfilice@csumb.edu>
2022-04-26 16:04:41 -0700

    Update Rename_Computer_Name_to_Serial.sh

--- Rename_Computer_Name_to_Serial.sh
+++ Rename_Computer_Name_to_Serial.sh
@@ -7,15 +7,38 @@
 # http://csumb.edu/it
 
 
-# This script will rename the computer name to serial number.
+# This script will rename the computer name to serial number or ComputerName value from Jamf Pro Extension Attributes.
+
 
 
 
 SCRIPTNAME=`/usr/bin/basename "$0"`
 SCRIPTPATH=`/usr/bin/dirname "$0"`
 
+# Jamf JSS Parameters 1 through 3 are predefined as mount point, computer name, and username
 
-/usr/local/bin/jamf setComputerName -useSerialNumber
+pathToScript=$0
+mountPoint=$1
+computerName=$2
+userName=$3
+
+shift 3
+# Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
+## Variables ##
+
+plistlocation="/Library/Managed Preferences/edu.csumb.custom.extensionattributes.plist"
+
+if [[ -e "$plistlocation" ]]; then
+	ComputerName=$(/usr/bin/defaults read "${plistlocation}" "ComputerName" 2>/dev/null)
+else
+	ComputerName=""
+fi
+
+if [ ! -z "${ComputerName}" ]; then
+	/usr/local/bin/jamf setComputerName -name "${ComputerName}"
+else
+	/usr/local/bin/jamf setComputerName -useSerialNumber
+fi
 
 /usr/local/bin/jamf recon
 
