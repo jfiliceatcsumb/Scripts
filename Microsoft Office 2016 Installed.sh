e3a90637fd775577b3014ff31ccc93aafea5204b
jfiliceatcsumb <jfilice@csumb.edu>
2022-05-02 13:06:43 -0700

    Create Microsoft Office 2016 Installed.sh

---
+++ Microsoft Office 2016 Installed.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# script to determine if MS Office 2016 is installed
+# https://www.jamf.com/jamf-nation/discussions/31041/microsoft-office-2019-extension-attribute
+
+excelversion=$(defaults read /Applications/Microsoft\ Excel.app/Contents/Info.plist CFBundleVersion)
+onenoteversion=$(defaults read /Applications/Microsoft\ OneNote.app/Contents/Info.plist CFBundleVersion)
+outlookversion=$(defaults read /Applications/Microsoft\ Outlook.app/Contents/Info.plist CFBundleVersion)
+powerpointversion=$(defaults read /Applications/Microsoft\ PowerPoint.app/Contents/Info.plist CFBundleVersion)
+wordversion=$(defaults read /Applications/Microsoft\ Word.app/Contents/Info.plist CFBundleVersion)
+
+
+######
+#TESTING AREA
+#echo "Excel version is $excelversion"
+#echo "OneNote version is $onenoteversion"
+#echo "Outlook version is $outlookversion"
+#echo "PowerPoint version is $powerpointversion"
+#echo "Word version is $wordversion"
+#
+#to check for a single app version, below will identify presence of EXCEL 15 through 16.16, including 16.0-9.x
+# if [[ ^15.*|^16.([0-9]\..*|1[0-6].*) ]];
+#if [[ $excelversion = 15.* || $excelversion = 16.[1-9].* || $excelversion = 16.1[0-6].* ]];
+#then echo "Excel 2016 Installed"
+#else echo "Excel 2016 Not Installed"
+#fi
+######
+
+
+#below will identify presence of any Office app version 15 through 16.16:
+if [[ $excelversion = ^15.*|^16.([0-9]\..*|1[0-6].*) || \
+$onenoteversion = ^15.*|^16.([0-9]\..*|1[0-6].*) || \
+$powerpointversion = ^15.*|^16.([0-9]\..*|1[0-6].*) || \
+$wordversion = ^15.*|^16.([0-9]\..*|1[0-6].*) ]] ;
+
+#For JAMF EA:
+then echo "<result>Office2016Installed</result>"
+else echo "<result>Office2016NotInstalled</result>"
+fi
+
+exit 0
+
