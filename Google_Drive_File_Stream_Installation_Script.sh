825df560fe463f2ecdc3ab0be9c95cbaa3d7ca48
jfiliceatcsumb <jfilice@csumb.edu>
2022-06-01 12:58:56 -0700

    Update Google_Drive_File_Stream_Installation_Script.sh

--- Google_Drive_File_Stream_Installation_Script.sh
+++ Google_Drive_File_Stream_Installation_Script.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/sh
+﻿#!/bin/bash
 
 ####################################################################################################
 #
@@ -12,41 +12,53 @@
 #
 ####################################################################################################
 # 
+set -x
 
 # Vendor supplied DMG file
-VendorDMG="GoogleDriveFileStream.dmg"
+VendorDMG="GoogleDrive.dmg"
 
-GoogleDriveFileStreamVolume="Install Google Drive File Stream"
+GoogleDriveVolume="Install Google Drive"
 
+
+# https://support.google.com/a/answer/7491144?hl=en&ref_topic=7455083#zippy=%2Cmac
+# Run the installer in silent mode:
+# hdiutil mount GoogleDrive.dmg; sudo installer -pkg /Volumes/Install\ Google\ Drive/GoogleDrive.pkg -target "/Volumes/Macintosh HD"; hdiutil unmount /Volumes/Install\ Google\ Drive/
 
 
 # Detatch any previous mounted installer image
-GoogleDriveFileStreamDMG="$(/usr/bin/hdiutil info | grep "/Volumes/$GoogleDriveFileStreamVolume" | awk '{ print $1 }')"
-if [ -n "$GoogleDriveFileStreamDMG" ]; then
-  /usr/bin/hdiutil detach $GoogleDriveFileStreamDMG
-fi
+for i in /Volumes/"${GoogleDriveVolume}"*
+do 
+	echo "$i"
+	if [[ -d "$i" ]]
+	then
+		/usr/bin/hdiutil detach "$i" -force
+	fi
+done
 
-# Reset value
-GoogleDriveFileStreamDMG=""
 
 # Download vendor supplied DMG file into /tmp/
-/usr/bin/curl https://dl.google.com/drive-file-stream/$VendorDMG -o /tmp/$VendorDMG
-
-# hdiutil mount GoogleDriveFileStream.dmg; sudo installer -pkg /Volumes/Install\ Google\ Drive\ File\ Stream/GoogleDriveFileStream.pkg -target "/Volumes/Macintosh HD"; hdiutil unmount /Volumes/Install\ Google\ Drive\ File\ Stream/
+/usr/bin/curl "https://dl.google.com/drive-file-stream/$VendorDMG" --location --output /tmp/$VendorDMG
+# https://dl.google.com/drive-file-stream/GoogleDrive.dmg
 
 # Mount vendor supplied DMG File
 /usr/bin/hdiutil attach /tmp/$VendorDMG -nobrowse
 
 
 # Identify the correct mount point for the vendor supplied DMG file 
-GoogleDriveFileStreamDMG="$(/usr/bin/hdiutil info | grep "/Volumes/$GoogleDriveFileStreamVolume" | awk '{ print $1 }')"
+# GoogleDriveFileStreamDMG="$(/usr/bin/hdiutil info | grep "/Volumes/$GoogleDriveVolume" | awk '{ print $1 }')"
 
 # Install
-# Preserve all file attributes and ACLs
-/usr/sbin/installer -pkg "/Volumes/$GoogleDriveFileStreamVolume/GoogleDriveFileStream.pkg" -target /
+/usr/sbin/installer -pkg "/Volumes/$GoogleDriveVolume/GoogleDrive.pkg" -target /
 
 # Unmount the vendor supplied DMG file
-/usr/bin/hdiutil detach $GoogleDriveFileStreamDMG
+for i in /Volumes/"${GoogleDriveVolume}"*
+do 
+	echo "$i"
+	if [[ -d "$i" ]]
+	then
+		/usr/bin/hdiutil detach "$i" -force
+	fi
+done
 
 # Remove the downloaded vendor supplied DMG file
 /bin/rm -f /tmp/$VendorDMG
