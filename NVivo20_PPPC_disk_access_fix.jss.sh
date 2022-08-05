4484aef7f7d8b04698447dba6f5bca914bd48774
jfiliceatcsumb <jfilice@csumb.edu>
2022-08-05 16:04:32 -0700

    Update NVivo20_PPPC_disk_access_fix.jss.sh

--- NVivo20_PPPC_disk_access_fix.jss.sh
+++ NVivo20_PPPC_disk_access_fix.jss.sh
@@ -31,10 +31,10 @@ userName=$3
 shift 3
 # Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
 
-echo pathToScript=$pathToScript
-echo mountPoint=$mountPoint
-echo computerName=$computerName
-echo userName=$userName
+echo "pathToScript=$pathToScript"
+echo "mountPoint=$mountPoint"
+echo "computerName=$computerName"
+echo "userName=$userName"
 
 # set alias for PlistBuddy and several others so I don't have to specify full path.
 # Prefix sudo path because I'm using it here for all commands.
@@ -54,10 +54,49 @@ alias sudo=/usr/bin/sudo
 # https://forums.nvivobyqsr.com/topic/10476-full-disk-access-issues-blocking-enterprise-installation-of-nvivo-12-and-nvivo-2020-on-macos-monterey-1231/
 # https://forums.nvivobyqsr.com/topic/11086-cant-access-nvivo-on-macbook-air-running-macos-monterey-124/
 
+# NVivo 20 expects there exists a file in the user's directory at 
 # ~/Library/Safari/CloudTabs.db
 
-# Example:
-# /bin/ls -FlOah "${SCRIPTDIR}"
+# ##### User Template #####
+osXversion=$(sw_vers -productVersion)
+echo "osXversion=$osXversion"
+# Just get the second version value after 10.
+macOSversionMajor=$(echo $osXversion | awk -F. '{print $1}')
+macOSversionMinor=$(echo $osXversion | awk -F. '{print $2}')
+echo "macOSversionMajor=$macOSversionMajor"
+echo "macOSversionMinor=$macOSversionMinor"
+
+# if 
+# macOS 11.x or newer
+# or
+# macOS 10.15.x or newer
+
+if [ $macOSversionMajor -gt 10 ] || [ $macOSversionMajor -eq 10 -a $macOSversionMinor -ge 15 ]; then
+	echo "10.15 and newer"
+	USER_TEMPL='/Library/User Template/Non_localized'
+else
+	echo "older than 10.15"
+	USER_TEMPL='/System/Library/User Template/Non_localized'
+fi
+
+echo "USER_TEMPL=$USER_TEMPL"
+
+/bin/mkdir -pv -m 755 "${USER_TEMPL}/Library/Safari"
+/usr/bin/touch "${USER_TEMPL}/Library/Safari/CloudTabs.db"
+/bin/chmod 644 "${USER_TEMPL}/Library/Safari/CloudTabs.db"
+/usr/sbin/chown 0:0 "${USER_TEMPL}/Library/Safari/CloudTabs.db"
+
+
+# ##### Current User #####
+if [ $userName != "" ]
+then
+	
+	/bin/mkdir -pv -m 755 "${userName}/Library/Safari"
+	/usr/bin/touch "${userName}/Library/Safari/CloudTabs.db"
+	/bin/chmod -f 644 "${userName}/Library/Safari/CloudTabs.db"
+	/usr/sbin/chown -fR $userName "${userName}/Library/Safari/"
+fi
+
 
 exit 0
 
