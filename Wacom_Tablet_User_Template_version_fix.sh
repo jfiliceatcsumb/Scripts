bb4c0ffe855dcc3a898b9f8d0776008d211db268
jfiliceatcsumb <jfilice@csumb.edu>
2022-08-19 10:28:04 -0700

    Update Wacom_Tablet_User_Template_version_fix.sh
    Update Wacom_Tablet_User_Template_version_fix.sh

--- Wacom_Tablet_User_Template_version_fix.sh
+++ Wacom_Tablet_User_Template_version_fix.sh
@@ -7,15 +7,17 @@
 # https://csumb.edu/it
 
 
-
-# This script expects Wacom Tablet Driver to be installed previously.
+# Script to work around an issue in which the Wacom Desktop Center.app launches when a user logs in. 
+# 		This happens by default unless turned off in the app's settings. 
+# 		This script sets the pref file in the User Template and applies to the currently logged in user. 
+# 		The currently logged in user may often be the account being used to provision the Mac. 
+# 
+# This script requires Wacom Desktop Center.app (Wacom Tablet Driver) to be installed previously. 
+# 		If solution requires the Wacom Desktop Center.app version to make this work.
+# 		If the script cannot read the version, then the script logic will bail out.
 # 
 # Use as script in Jamf JSS.
 
-
-# Change History:
-# 2022/08/16:	Creation.
-#
 
 SCRIPTNAME=`/usr/bin/basename "$0"`
 SCRIPTDIR=`/usr/bin/dirname "$0"`
@@ -44,13 +46,17 @@ echo "userName=$userName"
 # <key>LastShown</key>
 # <string>6.3.46-1</string>
 
+# set -x
 
 WacomPersistentPlist="/Library/Containers/com.wacom.DataStoreMgr/Data/Library/Preferences/.wacom/persistent.plist"
 WacomDesktopCenterVersion=""
 WacomDesktopCenterVersion=$(/usr/bin/defaults read "/Applications/Wacom Tablet.localized/Wacom Desktop Center.app/Contents/Info.plist" CFBundleShortVersionString)
+echo "WacomDesktopCenterVersion $WacomDesktopCenterVersion"
+
 
 if [[ "$WacomDesktopCenterVersion" != "" ]]
 then
+# ##### USER TEMPLATE #####
 	/bin/mkdir -p -m 755 "$(/usr/bin/dirname '/Library/User Template/Non_localized'${WacomPersistentPlist})" 
 	/usr/bin/defaults write "/Library/User Template/Non_localized${WacomPersistentPlist}" 'Analytics_On' -string "NO"
 	/usr/bin/defaults write "/Library/User Template/Non_localized${WacomPersistentPlist}" 'PEN_SECOND_RUN' -string "YES"
@@ -58,15 +64,23 @@ then
 	/usr/bin/defaults write "/Library/User Template/Non_localized${WacomPersistentPlist}" 'LastShown' -string "$WacomDesktopCenterVersion"
 	/bin/chmod 644 "/Library/User Template/Non_localized${WacomPersistentPlist}"
 	/usr/sbin/chown 0:0 "/Library/User Template/Non_localized${WacomPersistentPlist}"
+	echo "/Library/User Template/Non_localized${WacomPersistentPlist}..."
+    /usr/bin/defaults read "/Library/User Template/Non_localized${WacomPersistentPlist}" 
 
-	if [[  -d "/Users/$userName" ]]
+# ##### CURRENT LOGGED IN USER #####
+	if [[ "$userName" != "" ]]
 	then
-		/bin/mkdir -p -m 755 "$(/usr/bin/dirname /Users/$userName/${WacomPersistentPlist})" 
-		/usr/bin/defaults write "/Users/$userName/${WacomPersistentPlist}" 'Analytics_On' -string "NO"
-		/usr/bin/defaults write "/Users/$userName/${WacomPersistentPlist}" 'PEN_SECOND_RUN' -string "YES"
-		/usr/bin/defaults write "/Users/$userName/${WacomPersistentPlist}" 'TOUCH_SECOND_RUN' -string "YES"
-		/usr/bin/defaults write "/Users/$userName/${WacomPersistentPlist}" 'LastShown' -string "$WacomDesktopCenterVersion"
-		/usr/sbin/chown $userName "/Users/$userName/${WacomPersistentPlist}"
+		if [[  -d "/Users/$userName" ]]
+		then
+			/bin/mkdir -p -m 755 "$(/usr/bin/dirname /Users/$userName/${WacomPersistentPlist})" 
+			/usr/bin/defaults write "/Users/$userName/${WacomPersistentPlist}" 'Analytics_On' -string "NO"
+			/usr/bin/defaults write "/Users/$userName/${WacomPersistentPlist}" 'PEN_SECOND_RUN' -string "YES"
+			/usr/bin/defaults write "/Users/$userName/${WacomPersistentPlist}" 'TOUCH_SECOND_RUN' -string "YES"
+			/usr/bin/defaults write "/Users/$userName/${WacomPersistentPlist}" 'LastShown' -string "$WacomDesktopCenterVersion"
+			/usr/sbin/chown $userName "/Users/$userName/${WacomPersistentPlist}"
+			echo "/Users/$userName/${WacomPersistentPlist}..."
+            /usr/bin/defaults read "/Users/$userName/${WacomPersistentPlist}"
+		fi
 	fi
-	
 fi
+
