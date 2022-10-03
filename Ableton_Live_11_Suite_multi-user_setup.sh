7d5fd178a74b1fe779699be37adf08f5b0213f28
jfiliceatcsumb <jfilice@csumb.edu>
2022-10-03 12:29:37 -0700

    Update Ableton_Live_11_Suite_multi-user_setup.sh

--- Ableton_Live_11_Suite_multi-user_setup.sh
+++ Ableton_Live_11_Suite_multi-user_setup.sh
@@ -3,6 +3,8 @@
 # Installing Live in a Multi-User Environment (without Sassafras)
 # https://help.ableton.com/hc/en-us/articles/209073209-Installing-Live-in-a-Multi-User-Environment-without-Sassafras-
 
+set -x 
+
 Version="11.1.6"
 
 AbletonLiveApp="/Applications/Ableton Live 11 Suite.app"
@@ -20,7 +22,8 @@ mkdir -p -m 755 "/Library/Application Support/Ableton/Live $Version/Unlock/"
 # 2.2.3 Paste the Master Unlock.cfg file into the shared Unlock folder that you created in the step above.
 # 
 # Now, delete the Unlock folder in the default location. 
-# /Users/[Username]/Library/Application Support/Ableton/Live x.x.x./Unlock/
+# /Users/$USER/Library/Application Support/Ableton/Live $Version/Unlock/
+# /Users/admin/Library/Application Support/Ableton/Live 11.1.6/Unlock/
 
 # 2.2.4 Shared Preferences and Options.txt
 # 6.2 Example Options.txt
@@ -45,7 +48,8 @@ mkdir -p -m 775 "/Library/Application Support/Ableton/Factory Packs/"
 
 # Move the file titled Library.cfg (found in the user preferences) to the shared preferences folder (see 2.2.5). 
 # This way, information about the Ableton Live Packs you just installed will automatically be available to all users.
-# /Users/alex/Library/Preferences/Ableton/Live 11.1/
+# /Users/$USER/Library/Preferences/Ableton/Live $Version/
+# /Users/admin/Library/Preferences/Ableton/Live 11.1.6/
 # (Library.cfg, Options.txt, Log.txt)
 
 # /Library/Preferences/Ableton/Live $Version/Library.cfg
