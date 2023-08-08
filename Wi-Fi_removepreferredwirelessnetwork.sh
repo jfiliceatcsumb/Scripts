118961374b0c0f080a846cb48f94c821bba6a3cf
jfiliceatcsumb <jfilice@csumb.edu>
2023-08-07 18:48:37 -0700

    Create Wi-Fi_removepreferredwirelessnetwork.sh

---
+++ Wi-Fi_removepreferredwirelessnetwork.sh
@@ -0,0 +1,74 @@
+#!/bin/zsh
+
+# Jason Filice
+# jfilice@csumb.edu
+# Technology Support Services in IT
+# California State University, Monterey Bay
+# https://csumb.edu/it
+
+
+
+# This script requires .
+# Run it with one argument, string name of the wi-fi SSID to remove from preferred wireless networks. 
+# 
+# Use as script in Jamf JSS.
+
+
+# Change History:
+# 2023/08/07:	Creation.
+#
+
+SCRIPTNAME=`/usr/bin/basename "$0"`
+SCRIPTDIR=`/usr/bin/dirname "$0"`
+
+# Jamf JSS Parameters 1 through 3 are predefined as mount point, computer name, and username
+
+pathToScript=$0
+mountPoint=$1
+computerName=$2
+userName=$3
+
+shift 3
+# Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
+
+echo "pathToScript=$pathToScript"
+echo "mountPoint=$mountPoint"
+echo "computerName=$computerName"
+echo "userName=$userName"
+
+# set alias for PlistBuddy and several others so I don't have to specify full path.
+# Prefix sudo path because I'm using it here for all commands.
+# If I want to run a command without the alias, then specify the full path.
+alias PlistBuddy="/usr/libexec/PlistBuddy"
+alias chown="/usr/sbin/chown"
+alias chmod="/bin/chmod"
+alias ditto="/usr/bin/ditto"
+alias defaults="/usr/bin/defaults"
+alias rm="/bin/rm"
+alias cp="/bin/cp"
+alias mkdir="/bin/mkdir"
+alias sudo=/usr/bin/sudo
+
+# ##### Debugging flags #####
+# debug bash script by enabling verbose “-v” option
+# set -v
+# debug bash script using noexec (Test for syntaxt errors)
+# set -n
+# identify the unset variables while debugging bash script
+# set -u
+# debug bash script using xtrace
+# set -x
+
+# Example:
+#!/bin/sh
+
+SSID = "$1"
+
+for interface in $(networksetup -listnetworkserviceorder | grep Hardware| awk '/Wi-Fi/ { print $NF }'| awk -F ")"'{ print $1 }')
+do  
+	echo "Disconnecting $interface from non-internal device network"  
+	/usr/sbin/networksetup -removepreferredwirelessnetwork $interface "${SSID}"
+done
+
+exit 0
+
