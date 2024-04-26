64fbcaf2af5b2397ca8236e38ddd38f4931bdeca
jfiliceatcsumb <jfilice@csumb.edu>
2024-04-26 10:40:22 -0700

    Create Rave_Notifier_preinstall.jss.sh

---
+++ Rave_Notifier_preinstall.jss.sh
@@ -0,0 +1,62 @@
+#!/bin/zsh
+
+# Jason Filice
+# jfilice@csumb.edu
+# Technology Support Services in IT
+# California State University, Monterey Bay
+# https://csumb.edu/it
+
+
+
+# Preinstall script to delete existing application bundle.
+# For best results run preinstall script in a PKG installer.
+# 
+# Use as preinstall script in Jamf JSS.
+
+
+# Change History:
+# 2024/04/26:	Creation.
+#
+
+SCRIPTNAME=`/usr/bin/basename "$0"`
+SCRIPTDIR=`/usr/bin/dirname "$0"`
+
+# Jamf JSS Parameters 1 through 3 are predefined as mount point, computer name, and username
+
+pathToScript=$0
+mountPoint=$1
+computerName=$2
+userName=$3
+
+shift 3
+# Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
+
+echo "pathToScript=$pathToScript"
+echo "mountPoint=$mountPoint"
+echo "computerName=$computerName"
+echo "userName=$userName"
+
+
+# ##### Debugging flags #####
+# debug bash script by enabling verbose “-v” option
+# set -v
+# debug bash script using noexec (Test for syntaxt errors)
+# set -n
+# identify the unset variables while debugging bash script
+# set -u
+# debug bash script using xtrace
+# set -x
+
+# Example:
+# /bin/ls -FlOah "${SCRIPTDIR}"
+appBundlePath=""
+appBundleID="com.ale-enterprise.RaveNotifier" 
+
+if [[ -e "$appBundlePath" ]]; then
+	rm -fR "$appBundlePath"
+fi
+
+
+
+exit 0
+
