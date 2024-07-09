5f0efc45ae94bfcf84116c92b23781ed00e2d92f
jfiliceatcsumb <jfilice@csumb.edu>
2024-07-09 10:36:18 -0700

    Create Jamf_Run_policy_manual_trigger.zsh

---
+++ Jamf_Run_policy_manual_trigger.zsh
@@ -0,0 +1,10 @@
+﻿#!/bin/zsh
+
+# This script runs a manual policy trigger to
+# allow the policy or policies associated with that
+# trigger to be executed.
+
+trigger_name="$4"
+
+
+/usr/local/bin/jamf policy -verbose -event "$trigger_name"
