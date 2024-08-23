fae60307be7fbbbeb8da2ab0d9d066dc36dbd91c
jfiliceatcsumb <jfilice@csumb.edu>
2024-08-22 19:32:02 -0700

    Create Set_User_Template_AppleInterfaceStyle_Dark_Mode.jss.zsh

---
+++ Set_User_Template_AppleInterfaceStyle_Dark_Mode.jss.zsh
@@ -0,0 +1,68 @@
+#!/bin/zsh
+
+# Jason Filice
+# jfilice@csumb.edu
+# Technology Support Services in IT
+# California State University, Monterey Bay
+# https://csumb.edu/it
+
+
+
+# Sets the default user template Interface Style to Dark Mode.
+# Run it with no arguments. 
+# 
+# Use as script in Jamf JSS.
+
+
+# Change History:
+# 2024/08/22:	Creation.
+#
+
+SCRIPTNAME=`/usr/bin/basename "$0"`
+SCRIPTDIR=`/usr/bin/dirname "$0"`
+
+# Jamf JSS Parameters 1 through 3 are predefined as mount point, computer name, and username
+
+pathToScript=$0
+mountPoint=$1
+computerName=$2
+userName=$3
+
+shift 3
+# Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
+
+echo "pathToScript=$pathToScript"
+echo "mountPoint=$mountPoint"
+echo "computerName=$computerName"
+echo "userName=$userName"
+
+# set alias for PlistBuddy and several others so I don't have to specify full path.
+# Prefix sudo path because I'm using it here for all commands.
+# If I want to run a command without the alias, then specify the full path.
+alias PlistBuddy="/usr/libexec/PlistBuddy"
+alias chown="/usr/sbin/chown"
+alias chmod="/bin/chmod"
+alias ditto="/usr/bin/ditto"
+alias defaults="/usr/bin/defaults"
+alias rm="/bin/rm"
+alias cp="/bin/cp"
+alias mkdir="/bin/mkdir"
+alias sudo=/usr/bin/sudo
+
+# ##### Debugging flags #####
+# debug bash script by enabling verbose “-v” option
+# set -v
+# debug bash script using noexec (Test for syntaxt errors)
+# set -n
+# identify the unset variables while debugging bash script
+# set -u
+# debug bash script using xtrace
+# set -x
+
+/bin/mkdir -p -m 0755 "/System/Library/User Template/Non_localized/Library/Preferences/"
+/usr/bin/defaults write "/System/Library/User Template/Non_localized/Library/Preferences/.GlobalPreferences.plist" AppleInterfaceStyle -string "Dark"
+/bin/chmod 0:0 "/System/Library/User Template/Non_localized/Library/Preferences/.GlobalPreferences.plist"
+
+
+exit 0
+
