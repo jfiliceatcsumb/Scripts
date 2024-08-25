a4a40c7e731048518ffacea01a571a8f27cb60b3
jfiliceatcsumb <jfilice@csumb.edu>
2024-08-25 12:24:14 -0700

    Create LAB_WLC_Spanish-ISO_keyboard_User_Template.jss.zsh

---
+++ LAB_WLC_Spanish-ISO_keyboard_User_Template.jss.zsh
@@ -0,0 +1,82 @@
+#!/bin/zsh
+
+# Jason Filice
+# jfilice@csumb.edu
+# Technology Support Services in IT
+# California State University, Monterey Bay
+# https://csumb.edu/it
+
+
+
+# This script requires .
+# Run it with no arguments. 
+# 
+# Use as script in Jamf JSS.
+
+
+# Change History:
+# 2023/08/25:	Creation.
+#
+
+SCRIPTNAME=`/usr/bin/basename "$0"`
+SCRIPTDIR=`/usr/bin/dirname "$0"`
+
+# Jamf JSS Parameters 1 through 3 are predefined as mount point, computer name, and username
+
+pathToScript=$0
+mountPoint=$1
+computerName=$2
+userName=$3
+
+shift 3
+# Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
+
+echo "pathToScript=$pathToScript"
+echo "mountPoint=$mountPoint"
+echo "computerName=$computerName"
+echo "userName=$userName"
+
+# set alias for PlistBuddy and several others so I don't have to specify full path.
+# Prefix sudo path because I'm using it here for all commands.
+# If I want to run a command without the alias, then specify the full path.
+alias PlistBuddy="/usr/libexec/PlistBuddy"
+alias chown="/usr/sbin/chown"
+alias chmod="/bin/chmod"
+alias ditto="/usr/bin/ditto"
+alias defaults="/usr/bin/defaults"
+alias rm="/bin/rm"
+alias cp="/bin/cp"
+alias mkdir="/bin/mkdir"
+alias sudo=/usr/bin/sudo
+
+# ##### Debugging flags #####
+# debug bash script by enabling verbose “-v” option
+# set -v
+# debug bash script using noexec (Test for syntaxt errors)
+# set -n
+# identify the unset variables while debugging bash script
+# set -u
+# debug bash script using xtrace
+# set -x
+
+# Example:
+# /bin/ls -FlOah "${SCRIPTDIR}"
+
+
+/usr/bin/defaults write "/System/Library/User Template/Non_localized/Library/Preferences/com.apple.HIToolbox.plist" AppleEnabledInputSources -array-add \
+'<dict>
+<key>InputSourceKind</key>
+<string>Keyboard Layout</string>
+<key>KeyboardLayout ID</key>
+<integer>87</integer>
+<key>KeyboardLayout Name</key>
+<string>Spanish - ISO</string>
+</dict>'
+
+/usr/sbin/chown 0:0 "/System/Library/User Template/Non_localized/Library/Preferences/com.apple.HIToolbox.plist"
+
+/usr/sbin/chmod 644 "/System/Library/User Template/Non_localized/Library/Preferences/com.apple.HIToolbox.plist"
+
+
+exit 0
+
