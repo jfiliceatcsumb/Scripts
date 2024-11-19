8751d723b23b0f2f1bc2638aca854eccf90a0743
jfiliceatcsumb <jfilice@csumb.edu>
2024-11-18 18:04:12 -0800

    Update Microsoft Office 2021 Installed.sh

--- Microsoft Office 2021 Installed.sh
+++ Microsoft Office 2021 Installed.sh
@@ -36,12 +36,14 @@ fi
 #fi
 ######
 
+# Office LTSC for Mac 2024 has version numbers of 16.89 or higher. Office LTSC for Mac 2021 has version numbers of 16.53 or higher.
+# https://learn.microsoft.com/en-us/microsoft-365-apps/mac/overview?source=recommendations#version-numbers
 
-#below will identify presence of any Office app version 16.53 through 16.99:
-if [[ $excelversion =~ ^16.(5[3-9].*|[6-9][0-9].*) ]] || \
-[[ $onenoteversion =~ ^16.(5[3-9].*|[6-9][0-9].*) ]] || \
-[[ $powerpointversion =~ ^16.(5[3-9].*|[6-9][0-9].*) ]] || \
-[[ $wordversion =~ ^16.(5[3-9].*|[6-9][0-9].*) ]] ;
+#below will identify presence of any Office app version 16.53 through 16.88:
+if [[ $excelversion =~ ^16.(5[3-9].*|[6-7][0-9].*|8[0-8].*) ]] || \
+[[ $onenoteversion =~ ^16.(5[3-9].*|[6-7][0-9].*|8[0-8].*) ]] || \
+[[ $powerpointversion =~ ^16.(5[3-9].*|[6-7][0-9].*|8[0-8].*) ]] || \
+[[ $wordversion =~ ^16.(5[3-9].*|[6-7][0-9].*|8[0-8].*) ]] ;
 
 #For JAMF EA:
 then echo "<result>Office2021Installed</result>"
