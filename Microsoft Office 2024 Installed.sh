3bdfd4e50c58ce10cb2d1dc09bae756620224965
jfiliceatcsumb <jfilice@csumb.edu>
2024-11-18 18:12:09 -0800

    Update Microsoft Office 2024 Installed.sh

--- Microsoft Office 2024 Installed.sh
+++ Microsoft Office 2024 Installed.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# script to determine if MS Office 2021 is installed
-# https://www.jamf.com/jamf-nation/discussions/31041/microsoft-office-2019-extension-attribute
+# script to determine if MS Office 2024 is installed
 
 if [[ -e /Applications/Microsoft\ Excel.app/Contents/Info.plist ]]; then
 	excelversion=$(defaults read /Applications/Microsoft\ Excel.app/Contents/Info.plist CFBundleVersion)
@@ -39,15 +38,15 @@ fi
 # Office LTSC for Mac 2024 has version numbers of 16.89 or higher. Office LTSC for Mac 2021 has version numbers of 16.53 or higher.
 # https://learn.microsoft.com/en-us/microsoft-365-apps/mac/overview?source=recommendations#version-numbers
 
-#below will identify presence of any Office app version 16.53 through 16.99:
-if [[ $excelversion =~ ^16.(5[3-9].*|[6-9][0-9].*) ]] || \
-[[ $onenoteversion =~ ^16.(5[3-9].*|[6-9][0-9].*) ]] || \
-[[ $powerpointversion =~ ^16.(5[3-9].*|[6-9][0-9].*) ]] || \
-[[ $wordversion =~ ^16.(5[3-9].*|[6-9][0-9].*) ]] ;
+#below will identify presence of any Office app version 16.89 through 16.99:
+if [[ $excelversion =~ ^16.(89.*|9[0-9].*) ]] || \
+[[ $onenoteversion =~ ^16.(89.*|9[0-9].*) ]] || \
+[[ $powerpointversion =~ ^16.(89.*|9[0-9].*) ]] || \
+[[ $wordversion =~ ^16.(89.*|9[0-9].*) ]] ;
 
 #For JAMF EA:
-then echo "<result>Office2021Installed</result>"
-else echo "<result>Office2021NotInstalled</result>"
+then echo "<result>Office2024Installed</result>"
+else echo "<result>Office2024NotInstalled</result>"
 fi
 
 exit 0
