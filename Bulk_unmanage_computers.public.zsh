36408fe3d896721cfbef64af9e4009fd25a66a2d
jfiliceatcsumb <jfilice@csumb.edu>
2025-01-30 13:35:57 -0800

    Update Bulk_unmanage_computers.public.zsh

--- Bulk_unmanage_computers.public.zsh
+++ Bulk_unmanage_computers.public.zsh
@@ -17,7 +17,7 @@ SCRIPTNAME=`/usr/bin/basename "$0"`
 SCRIPTDIR=`/usr/bin/dirname "$0"`
 
 # ################### Variables ###################
-URL=""
+url=""
 client_id=""
 client_secret=""
 
@@ -29,18 +29,48 @@ SN#####
 )
 # ################### ###################
 
-# set alias for PlistBuddy and several others so I don't have to specify full path.
-# Prefix sudo path because I'm using it here for all commands.
-# If I want to run a command without the alias, then specify the full path.
-alias PlistBuddy="/usr/libexec/PlistBuddy"
-alias chown="/usr/sbin/chown"
-alias chmod="/bin/chmod"
-alias ditto="/usr/bin/ditto"
-alias defaults="/usr/bin/defaults"
-alias rm="/bin/rm"
-alias cp="/bin/cp"
-alias mkdir="/bin/mkdir"
-alias sudo=/usr/bin/sudo
+
+url="https://yourserver.jamfcloud.com"
+client_id="your-client-id"
+client_secret="yourClientSecret"
+
+getAccessToken() {
+	response=$(curl --silent --location --request POST "${url}/api/oauth/token" \
+ 	 	--header "Content-Type: application/x-www-form-urlencoded" \
+ 		--data-urlencode "client_id=${client_id}" \
+ 		--data-urlencode "grant_type=client_credentials" \
+ 		--data-urlencode "client_secret=${client_secret}")
+ 	access_token=$(echo "$response" | plutil -extract access_token raw -)
+ 	token_expires_in=$(echo "$response" | plutil -extract expires_in raw -)
+ 	token_expiration_epoch=$(($current_epoch + $token_expires_in - 1))
+}
+
+checkTokenExpiration() {
+ 	current_epoch=$(date +%s)
+    if [[ token_expiration_epoch -ge current_epoch ]]
+    then
+        echo "Token valid until the following epoch time: " "$token_expiration_epoch"
+    else
+        echo "No valid token available, getting new token"
+        getAccessToken
+    fi
+}
+
+invalidateToken() {
+	responseCode=$(curl -w "%{http_code}" -H "Authorization: Bearer ${access_token}" $url/api/v1/auth/invalidate-token -X POST -s -o /dev/null)
+	if [[ ${responseCode} == 204 ]]
+	then
+		echo "Token successfully invalidated"
+		access_token=""
+		token_expiration_epoch="0"
+	elif [[ ${responseCode} == 401 ]]
+	then
+		echo "Token already invalid"
+	else
+		echo "An unknown error occurred invalidating the token"
+	fi
+}
+
 
 # ##### Debugging flags #####
 # debug bash script by enabling verbose “-v” option
@@ -58,41 +88,43 @@ alias sudo=/usr/bin/sudo
 echo "Enter JSS URL (with port # if not 443)"
 echo "e.g. https://mycompany.jamfcloud.com:443"
 echo "URL:"
-read URL
-echo "Enter JSS username:"
-read USERNAME
-echo "Enter JSS password:"
-read -s PASSWORD
+read url
+echo "API client ID:"
+read client_id
+echo "API client secret:"
+read -s client_secret
  
-TOKEN_EXPIRATION_EPOCH="0"
+checkTokenExpiration
+curl -H "Authorization: Bearer ${access_token}" $url/api/v1/jamf-pro-version -X GET
+checkTokenExpiration
 
 
 for SERIAL in ${unmanage[@]}
 do
  
-# This next commented code is to get the serial number of the Mac from which the script 
-# is running in the case of performing the script on this local Mac to remove it from management.
-# I've turned it off in favour of using an array of provided SNs of other Macs. See above.
-# to remove from management.
- 
-# Get local serial number:
- 
-# SERIAL=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
-# /bin/echo "Serial number is $SERIAL"
- 
-# Get JAMF ID of device from API looked by SN found locally or provided in
-# $unmanage array:
-JAMF_ID=$(curl -X GET "${URL}/JSSResource/computers/serialnumber/$SERIAL" -H "accept: application/xml" -H "Authorization: Bearer $BEARER_TOKEN" | xmllint --xpath '/computer/general/id/text()' -)
- 
-# API call to de-select "Allow Jamf Pro to perform management tasks" in the JSS for this device:
-curl --request PUT --url "${URL}/JSSResource/computers/id/$JAMF_ID" -H "Content-Type: application/xml" -H "Accept: application/xml" -H "Authorization: Bearer $BEARER_TOKEN" -d '<computer><general><remote_management><managed>false</managed></remote_management></general></computer>'
- 
-/bin/echo "JAMF ID for $SERIAL is $JAMF_ID and it is now unmanaged in the JSS"
+	# This next commented code is to get the serial number of the Mac from which the script 
+	# is running in the case of performing the script on this local Mac to remove it from management.
+	# I've turned it off in favour of using an array of provided SNs of other Macs. See above.
+	# to remove from management.
+	 
+	# Get local serial number:
+	 
+	# SERIAL=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+	# /bin/echo "Serial number is $SERIAL"
+	 
+	# Get JAMF ID of device from API looked by SN found locally or provided in
+	# $unmanage array:
+	JAMF_ID=$(curl -X GET "${url}/JSSResource/computers/serialnumber/$SERIAL" -H "accept: application/xml" -H "Authorization: Bearer ${access_token}" | xmllint --xpath '/computer/general/id/text()' -)
+	 
+	# API call to de-select "Allow Jamf Pro to perform management tasks" in the JSS for this device:
+	curl --request PUT --url "${url}/JSSResource/computers/id/$JAMF_ID" -H "Content-Type: application/xml" -H "Accept: application/xml" -H "Authorization: Bearer ${access_token}" -d '<computer><general><remote_management><managed>false</managed></remote_management></general></computer>'
+	 
+	/bin/echo "JAMF ID for $SERIAL is $JAMF_ID and it is now unmanaged in the JSS"
 done
  
-# Bin the token
 /bin/echo "Invalidating API token..."
 invalidateToken
+curl -H "Authorization: Bearer ${access_token}" $url/api/v1/jamf-pro-version -X GET
  
 /bin/echo "Done."
  
