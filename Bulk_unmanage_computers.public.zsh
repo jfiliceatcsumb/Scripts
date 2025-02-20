f4fb6b375b63309ebd9623f86186b2fd750927b0
jfiliceatcsumb <jfilice@csumb.edu>
2025-02-20 15:19:05 -0800

    Update Bulk_unmanage_computers.public.zsh

--- Bulk_unmanage_computers.public.zsh
+++ Bulk_unmanage_computers.public.zsh
@@ -14,9 +14,9 @@ SCRIPTNAME=`/usr/bin/basename "$0"`
 SCRIPTDIR=`/usr/bin/dirname "$0"`
 
 # ################### Variables ###################
-url=""
-client_id=""
-client_secret=""
+url="" # https://yourserver.jamfcloud.com
+client_id="" # your-client-id
+client_secret="" # yourClientSecret
 
 # Paste in a list of Mac SNs to be removed from management:
 # Create an empty array to store the data
@@ -28,10 +28,6 @@ SN#####
 )
 # ################### ###################
 
-
-url="https://yourserver.jamfcloud.com"
-client_id="your-client-id"
-client_secret="yourClientSecret"
 
 getAccessToken() {
 	response=$(curl --silent --location --request POST "${url}/api/oauth/token" \
@@ -56,7 +52,7 @@ checkTokenExpiration() {
 }
 
 invalidateToken() {
-	responseCode=$(curl -w "%{http_code}" -H "Authorization: Bearer ${access_token}" $url/api/v1/auth/invalidate-token -X POST -s -o /dev/null)
+	responseCode=$(curl -w "%{http_code}" --header "Authorization: Bearer ${access_token}" $url/api/v1/auth/invalidate-token --request POST -s -o /dev/null)
 	if [[ ${responseCode} == 204 ]]
 	then
 		echo "Token successfully invalidated"
@@ -96,26 +92,29 @@ done < "$csv_file"
 # This can't be run from Jamf. We're just storing it here.
 # Download it to your Mac and run it in Terminal.
  
-echo "Enter JSS URL (with port # if not 443)"
-echo "e.g. https://mycompany.jamfcloud.com:443"
-echo "URL:"
-read url
-echo "API client ID:"
-read client_id
-echo "API client secret:"
-read -s client_secret
-echo "Enter file path to CSV file with list of Mac serial numbers to be removed from management:"
-read csv_file
- 
+date
+
+if [[ "${url}" = "" ]] ||  [[ "${client_id}" = "" ]]  || [[ "${client_secret}" = "" ]]; then
+	echo "Enter JSS URL (with port # if not 443)"
+	echo "e.g. https://mycompany.jamfcloud.com:443"
+	echo "URL:"
+	read url
+	echo "API client ID:"
+	read client_id
+	echo "API client secret:"
+	read -s client_secret
+	echo "Enter file path to CSV file with list of Mac serial numbers to be removed from management:"
+	read csv_file
+fi
 checkTokenExpiration
-curl -H "Authorization: Bearer ${access_token}" $url/api/v1/jamf-pro-version -X GET
+curl --header "Authorization: Bearer ${access_token}" $url/api/v1/jamf-pro-version --request GET
 checkTokenExpiration
 
 readCSVtoArray
 
 for SERIAL in ${unmanage_array[@]}
 do
- 
+ 	checkTokenExpiration
 	# This next commented code is to get the serial number of the Mac from which the script 
 	# is running in the case of performing the script on this local Mac to remove it from management.
 	# I've turned it off in favour of using an array of provided SNs of other Macs. See above.
@@ -128,19 +127,21 @@ do
 	 
 	# Get JAMF ID of device from API looked by SN found locally or provided in
 	# $unmanage_array array:
-	JAMF_ID=$(curl -X GET "${url}/JSSResource/computers/serialnumber/$SERIAL" -H "accept: application/xml" -H "Authorization: Bearer ${access_token}" | xmllint --xpath '/computer/general/id/text()' -)
+	JAMF_ID=$(curl --request GET "${url}/JSSResource/computers/serialnumber/$SERIAL" --header "accept: application/xml" --header "Authorization: Bearer ${access_token}" | xmllint --xpath '/computer/general/id/text()' -)
 	 
 	# API call to de-select "Allow Jamf Pro to perform management tasks" in the JSS for this device:
-	curl --request PUT --url "${url}/JSSResource/computers/id/$JAMF_ID" -H "Content-Type: application/xml" -H "Accept: application/xml" -H "Authorization: Bearer ${access_token}" -d '<computer><general><remote_management><managed>false</managed></remote_management></general></computer>'
-	 
+	curl --request PUT --url "${url}/JSSResource/computers/id/$JAMF_ID" --header "Content-Type: application/xml" --header "Accept: application/xml" --header "Authorization: Bearer ${access_token}" --data '<computer><general><remote_management><managed>false</managed></remote_management></general></computer>'
+	echo 
 	/bin/echo "JAMF ID for $SERIAL is $JAMF_ID and it is now unmanaged in the JSS"
 done
+# ##### LOOP DONE #####
  
 /bin/echo "Invalidating API token..."
 invalidateToken
-curl -H "Authorization: Bearer ${access_token}" $url/api/v1/jamf-pro-version -X GET
+/bin/echo "Confirm API token invalidated..."
+curl --header "Authorization: Bearer ${access_token}" $url/api/v1/jamf-pro-version --request GET
  
 /bin/echo "Done."
- 
+date
 exit 0
 
