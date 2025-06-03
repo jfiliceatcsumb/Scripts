50a37d4d285765f565d8263352d3c67f7a2c1599
jfiliceatcsumb <jfilice@csumb.edu>
2025-06-02 18:01:51 -0700

    Create Techsmith_Snagit_LicenseKey copy.jss.zsh

---
+++ Techsmith_Snagit_LicenseKey copy.jss.zsh
@@ -0,0 +1,63 @@
+#!/bin/zsh --no-rcs
+
+# Jason Filice
+# jfilice@csumb.edu
+# Technology Support Services in IT
+# California State University, Monterey Bay
+# https://csumb.edu/it
+
+
+
+# This script requires one argument: LicenseKey value.
+# Run it with no arguments. 
+# 
+# Use as script in Jamf JSS.
+
+# https://support.techsmith.com/hc/en-us/articles/203727638-Enterprise-Install-Guidelines-For-Camtasia-on-macOS
+# key file path: /Users/Shared/TechSmith/Camtasia/LicenseKey
+#
+
+SCRIPTNAME=`/usr/bin/basename "$0"`
+SCRIPTDIR=`/usr/bin/dirname "$0"`
+
+# Jamf JSS Parameters 1 through 3 are predefined as mount point, computer name, and username
+
+pathToScript=$0
+mountPoint=$1
+computerName=$2
+userName=$3
+
+shift 3
+# Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
+
+echo "pathToScript=$pathToScript"
+echo "mountPoint=$mountPoint"
+echo "computerName=$computerName"
+echo "userName=$userName"
+
+
+# ##### Debugging flags #####
+# debug bash script by enabling verbose “-v” option
+# set -v
+# debug bash script using noexec (Test for syntaxt errors)
+# set -n
+# identify the unset variables while debugging bash script
+# set -u
+# debug bash script using xtrace
+# set -x
+
+# Example:
+# /bin/ls -FlOah "${SCRIPTDIR}"
+echo $LANG
+echo "export LANG=en_US.UTF-8"
+export LANG=en_US.UTF-8
+echo $LANG
+
+LicenseKey="${1}"
+/bin/mkdir -pv -m 777 "/Users/Shared/TechSmith/Camtasia"
+echo "$LicenseKey" > "/Users/Shared/TechSmith/Camtasia/LicenseKey"
+/bin/chmod -fv 644 "/Users/Shared/TechSmith/Camtasia/LicenseKey"
+ls -FlOah "/Users/Shared/TechSmith/Camtasia"
+
+exit 0
+
