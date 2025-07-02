2910935351257ff7e0ca7705e005ce0d6ad75bb0
jfiliceatcsumb <jfilice@csumb.edu>
2025-07-01 18:34:52 -0700

    Update Bootstraptoken_Escrow_Fix.sh

--- Bootstraptoken_Escrow_Fix.sh
+++ Bootstraptoken_Escrow_Fix.sh
@@ -33,19 +33,6 @@ echo "mountPoint=$mountPoint"
 echo "computerName=$computerName"
 echo "userName=$userName"
 
-# set alias for PlistBuddy and several others so I don't have to specify full path.
-# Prefix sudo path because I'm using it here for all commands.
-# If I want to run a command without the alias, then specify the full path.
-alias PlistBuddy="/usr/libexec/PlistBuddy"
-alias chown="/usr/sbin/chown"
-alias chmod="/bin/chmod"
-alias ditto="/usr/bin/ditto"
-alias defaults="/usr/bin/defaults"
-alias rm="/bin/rm"
-alias cp="/bin/cp"
-alias mkdir="/bin/mkdir"
-alias sudo=/usr/bin/sudo
-
 # ##### Debugging flags #####
 # debug bash script by enabling verbose “-v” option
 # set -v
@@ -67,7 +54,7 @@ if [[ $bootstrap == *"supported on server: YES"* ]]; then
     else
 		echo "Bootstrap not escrowed."
 		echo "Creating the Bootstrap Token APFS record and escrowing to the MDM server..."
-        /usr/bin/profiles install -type bootstraptoken -user "${1}" -password "${2}"
+        /usr/bin/profiles install -type bootstraptoken -user "${1}" -password "${2}" -verbose
 		sleep 1	
 		/usr/bin/profiles status -type bootstraptoken
   fi
