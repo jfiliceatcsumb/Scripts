85c15750e2746ba5863579c7d6f70d6788270294
jfiliceatcsumb <jfilice@csumb.edu>
2025-12-01 17:44:05 -0800

    Update softwareupdate_04_install_items.jss.zsh

--- softwareupdate_04_install_items.jss.zsh
+++ softwareupdate_04_install_items.jss.zsh
@@ -8,7 +8,16 @@
 
 
 
-# Run it with software update identifiers arguments. The script uses quoting to keep spaces in the identifier names.
+# Run it with software update identifiers arguments. 
+# 4:	RESTART = TRUE | FALSE
+# 5:	ASU update label
+# 6:	[ASU update label]
+# 7:	[ASU update label]
+# 8:	[ASU update label]
+# 9:	[ASU update label]
+# 10:	[ASU update label]
+# 11:	[ASU update label]
+# The script uses quoting to keep spaces in the identifier names.
 # 
 # Use as script in Jamf JSS.
 
@@ -28,6 +37,15 @@ userName=$3
 shift 3
 # Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
 
+RESTART=""
+if [[ "${1}" == "FALSE" ]]; then
+	RESTART="FALSE"
+else
+	RESTART="TRUE"
+fi
+# Shift off parameter #4 so that we are only left with parameters for update labels
+shift
+
 echo "List all available updates..."
 /usr/sbin/softwareupdate --list
 
@@ -38,42 +56,18 @@ do
 	if [ "$1" != "" ]
 	then
 		echo "Downloading update for ${1}..."
-        /usr/sbin/softwareupdate --download "$1"
+		/usr/sbin/softwareupdate --download "$1"
 		sleep 1
-		echo "Installing update for ${1}..."
-		/usr/sbin/softwareupdate --install "$1"
+		if [[ "${RESTART}" == "TRUE" ]]; then
+			echo "Installing update for ${1} (restart if required)..."
+			/usr/sbin/softwareupdate --install "$1" --no-scan --agree-to-license --restart
+		else
+			echo "Installing update for ${1}..."
+			/usr/sbin/softwareupdate --install "$1" --no-scan --agree-to-license
+		fi
 	fi
 	shift
 done
 
 exit 0
-
-
-# https://developer.apple.com/library/mac/documentation/darwin/reference/manpages/man8/softwareupdate.8.html
-# The --list output shows the item names you can specify here, prefixed by the * or - characters.
-#
-#     --ignore identifier ...
-#                 Manages the per-machine list of ignored updates. The identifier is the first part of the
-#                 item name (before the dash and version number) that is shown by --list.  See EXAMPLES.
-# EXAMPLES
-#      The following examples are shown as given to the shell:
-# 
-#      softwareupdate --list
-# 
-#            Software Update Tool
-#            Copyright 2002-2012 Apple Inc.
-# 
-#            Finding available software
-#            Software Update found the following new or updated software:
-#               * MacBookAirEFIUpdate2.4-2.4
-#                    MacBook Air EFI Firmware Update (2.4), 3817K [recommended] [restart]
-#               * ProAppsQTCodecs-1.0
-#                    ProApps QuickTime codecs (1.0), 968K [recommended]
-#               * JavaForOSX-1.0
-#                    Java for OS X 2012-005 (1.0), 65288K [recommended]
-#      sudo softwareupdate --ignore JavaForOSX
-# 
-#            Ignored updates:
-#            (JavaForOSX)
-# 
 
