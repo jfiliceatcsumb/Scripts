3e2b54d604bf4e992d543949cbe45584f668ac51
jfiliceatcsumb <jfilice@csumb.edu>
2026-03-11 16:58:42 -0700

    Update open_system_settings_pane.jss.zsh

--- open_system_settings_pane.jss.zsh
+++ open_system_settings_pane.jss.zsh
@@ -1,4 +1,4 @@
-﻿#!/bin/sh
+﻿#!/bin/zsh --no-rcs
 
 # Jason Filice
 # jfilice@csumb.edu
@@ -31,9 +31,14 @@ userName=$3
 shift 3
 # Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
 
-# Find the path to SoftwareUpdate.prefPane
-PrefPane=""
-PrefPane="${1}"
+# Provide the URLScheme for the pref pane after x-apple.systempreferences:'
+# e.g.: 
+# com.apple.preference.universalaccess
+# com.apple.Accessibility-Settings.extension
+# com.apple.preference.security?Privacy_LocationServices
+# If left empty, then it will just open the System Preferences to the front.
+PrefPaneURLScheme=""
+PrefPaneURLScheme="${1}"
 
 # convenience function to run a command as the current user
 # https://scriptingosx.com/2020/08/running-a-command-as-another-user/
@@ -50,10 +55,9 @@ runAsUser() {
   fi
 }
 
-/usr/bin/su -l "$userName" -c "/usr/bin/open -b com.apple.systempreferences"
-/usr/bin/su -l "$userName" -c "/usr/bin/open x-apple.systempreferences:com.apple.preference.universalaccess"
-/usr/bin/su -l "$userName" -c "/usr/bin/open x-apple.systempreferences:com.apple.Accessibility-Settings.extension"
-/usr/bin/su -l "$userName" -c "/usr/bin/open -b com.apple.systempreferences"
+runAsUser /usr/bin/open -b com.apple.systempreferences
+runAsUser /usr/bin/open "x-apple.systempreferences:${PrefPaneURLScheme}"
+runAsUser /usr/bin/open -b com.apple.systempreferences
 
 # -n  
 # 		Open a new instance of the application(s) even if one is already running.
