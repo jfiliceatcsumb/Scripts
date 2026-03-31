30d08e540c7b27782ca2393d0dcf72d58ab383dc
jfiliceatcsumb <jfilice@csumb.edu>
2026-03-31 01:19:59 -0700

    Create Set_User_Template_Tahoe_icons-in-menus.jss.zsh

---
+++ Set_User_Template_Tahoe_icons-in-menus.jss.zsh
@@ -0,0 +1,61 @@
+#!/bin/zsh
+
+# Jason Filice
+# jfilice@csumb.edu
+# Technology Support Services in IT
+# California State University, Monterey Bay
+# https://csumb.edu/it
+
+
+
+# Sets the default user template to turn off menu icons in macOS Tahoe..
+# https://mastodon.social/@stroughtonsmith/116262411548746327
+# Run it with no arguments. 
+# 
+# Use as script in Jamf JSS.
+
+
+# Change History:
+# 2026/03/31:	Creation.
+#
+
+SCRIPTNAME=`/usr/bin/basename "$0"`
+SCRIPTDIR=`/usr/bin/dirname "$0"`
+
+# Jamf JSS Parameters 1 through 3 are predefined as mount point, computer name, and username
+
+pathToScript=$0
+mountPoint=$1
+computerName=$2
+userName=$3
+
+shift 3
+# Shift off the $1 $2 $3 parameters passed by the JSS so that parameter 4 is now $1
+
+echo "pathToScript=$pathToScript"
+echo "mountPoint=$mountPoint"
+echo "computerName=$computerName"
+echo "userName=$userName"
+
+
+# ##### Debugging flags #####
+# debug bash script by enabling verbose “-v” option
+# set -v
+# debug bash script using noexec (Test for syntaxt errors)
+# set -n
+# identify the unset variables while debugging bash script
+# set -u
+# debug bash script using xtrace
+# set -x
+
+/bin/mkdir -p -m 0755 "/Library/User Template/Non_localized/Library/Preferences/"
+/usr/bin/defaults write "/Library/User Template/Non_localized/Library/Preferences/.GlobalPreferences.plist" NSMenuEnableActionImages -bool NO
+/usr/sbin/chown 0:0 "/Library/User Template/Non_localized/Library/Preferences/.GlobalPreferences.plist"
+
+echo "read GlobalPreferences.plist"
+/usr/bin/defaults read "/Library/User Template/Non_localized/Library/Preferences/.GlobalPreferences.plist"
+
+
+exit 
+
+
