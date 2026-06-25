32874f6f649a5c13f5f0f4cdc4dd22dde3e30d85
jfiliceatcsumb <jfilice@csumb.edu>
2026-06-24 17:11:17 -0700

    Update xcode-cli-tools_Workbrew.jss.zsh

--- xcode-cli-tools_Workbrew.jss.zsh
+++ xcode-cli-tools_Workbrew.jss.zsh
@@ -14,13 +14,10 @@ if [[ ! -f "/Library/Developer/CommandLineTools/usr/bin/git" ]]; then
 	echo "Xcode Command Line Tools not found. Starting installation..."
   CLT_PLACEHOLDER="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
   /usr/bin/sudo /usr/bin/touch "${CLT_PLACEHOLDER}"
-  CLT_PACKAGE="$(softwareupdate -l | grep -B 1 "Command Line Tools" | awk -F"*" '/^ *\*/ {print $2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1)"
-  /usr/bin/sudo /usr/sbin/softwareupdate --install --verbose "${CLT_PACKAGE}"
-  /usr/bin/sudo /bin/rm -vf "${CLT_PLACEHOLDER}"
-else
-	echo "Xcode Command Line Tools are already installed. Checking for available updates..."
-  CLT_PACKAGE="$(softwareupdate -l | grep -B 1 "Command Line Tools" | awk -F"*" '/^ *\*/ {print $2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1)"
-  /usr/bin/sudo /usr/sbin/softwareupdate --install --verbose "${CLT_PACKAGE}"
 fi
+	echo "Xcode Command Line Tools are already installed. Checking for available updates..."
+  CLT_PACKAGE="$(/usr/sbin/softwareupdate -l | grep -B 1 "Command Line Tools" | awk -F"*" '/^ *\*/ {print $2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1)"
+  /usr/bin/sudo /usr/sbin/softwareupdate --install --verbose "${CLT_PACKAGE}"
+	/usr/bin/sudo /bin/rm -vf "${CLT_PLACEHOLDER}"  2>/dev/null
 
 exit
