187552e8e86852d50b78ad12c0d1530c5f5b0b3b
jfiliceatcsumb <jfilice@csumb.edu>
2026-07-10 14:09:41 -0700

    Add macOS version check and improve CLI tools installation
    Add macOS 10.13.4+ version validation and enhanced Xcode CLI tools installation logic. Changes include: enabling debug mode, detecting macOS version, validating minimum requirements, improving package detection with additional softwareupdate flags, and adding better handling for existing installations.

--- xcode-cli-tools_Workbrew.jss.zsh
+++ xcode-cli-tools_Workbrew.jss.zsh
@@ -2,22 +2,41 @@
 
 # https://workbrew.com/docs/deployment-guides/workbrew-deployment-guide-jamf-pro
 
-# set -x
+set -x
 
 # Get and install Xcode CLI tools
-# Prerequisites: macOS 10.9.x or newer
+# Prerequisites: macOS 10.13.4 or newer
+
+macOSversion=$(sw_vers -productVersion)
+echo "macOSversion=${macOSversion}"
+# Just get the second version value after 10.
+macOSversionMajor=$(echo ${macOSversion} | awk -F. '{print $1}')
+macOSversionMinor=$(echo ${macOSversion} | awk -F. '{print $2}')
+macOSversionMinorUpdate=$(echo ${macOSversion} | awk -F. '{print $3}')
+echo "macOSversionMajor=${macOSversionMajor}"
+echo "macOSversionMinor=${macOSversionMinor}"
+echo "macOSversionMinorUpdate=${macOSversionMinorUpdate}"
 
 
+if [[ ${macOSversionMajor} -eq 10 -a ${macOSversionMinor} -lt 13 ] || [ ${macOSversionMajor} -eq 10 -a ${macOSversionMinor} -eq 13 -a ${macOSversionMinorUpdate} -lt 4 ]]; then
+	echo "ERROR: Prerequisites macOS 10.13.4 or newer. Terminating script." 1>&2
+	exit 1
+fi
 # on 10.9+, we can leverage SUS to get the latest CLI tools
-
+exit 0
+CLT_PLACEHOLDER="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
 if [[ ! -f "/Library/Developer/CommandLineTools/usr/bin/git" ]]; then
 	echo "Xcode Command Line Tools not found. Starting installation..."
-  CLT_PLACEHOLDER="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
   /usr/bin/sudo /usr/bin/touch "${CLT_PLACEHOLDER}"
-fi
+  sleep 1
+else
 	echo "Xcode Command Line Tools are already installed. Checking for available updates..."
-  CLT_PACKAGE="$(/usr/sbin/softwareupdate -l | grep -B 1 "Command Line Tools" | awk -F"*" '/^ *\*/ {print $2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1)"
-  /usr/bin/sudo /usr/sbin/softwareupdate --install --verbose "${CLT_PACKAGE}"
+	/usr/bin/sudo /bin/rm -vf "${CLT_PLACEHOLDER}"  2>/dev/null
+  sleep 1
+fi
+  CLT_PACKAGE=$(/usr/sbin/softwareupdate --list --force --verbose | grep -B 1 "Command Line Tools")
+  CLT_PACKAGE=$(echo "$CLT_PACKAGE" | awk -F"*" '/^ *\*/ {print $2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1)
+  /usr/bin/sudo /usr/sbin/softwareupdate --install --no-scan --force --agree-to-license --verbose "${CLT_PACKAGE}"
 	/usr/bin/sudo /bin/rm -vf "${CLT_PLACEHOLDER}"  2>/dev/null
 
 exit
