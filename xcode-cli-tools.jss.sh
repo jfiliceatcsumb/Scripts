cc4c6f68188c386db375646d0006a63f9710109a
jfiliceatcsumb <jfilice@csumb.edu>
2026-07-10 14:09:17 -0700

    Use [[ ]] instead of [ ] for test operators
    Replace POSIX-style single bracket test operators with bash-native double bracket operators. The [[ ]] construct provides better handling of variables and is the modern bash standard for conditionals.

--- xcode-cli-tools.jss.sh
+++ xcode-cli-tools.jss.sh
@@ -21,7 +21,7 @@ echo "macOSversionMinor=$macOSversionMinor"
 # macOS 10.9.x or newer
 
 # on 10.9+, we can leverage SUS to get the latest CLI tools
-if [ $macOSversionMajor -gt 11 ] || [ $macOSversionMajor -eq 10 -a $macOSversionMinor -ge 9 ]; then
+if [[ $macOSversionMajor -gt 11 ]] || [[ $macOSversionMajor -eq 10 -a $macOSversionMinor -ge 9 ]]; then
 	# https://stackoverflow.com/questions/15371925/how-to-check-if-command-line-tools-is-installed
 	echo "Check if command line tools are installed..."
 	/usr/bin/xcode-select -p 1>/dev/null 2>&1 
@@ -53,14 +53,14 @@ if [ $macOSversionMajor -gt 11 ] || [ $macOSversionMajor -eq 10 -a $macOSversion
 # the dvtdownloadableindex:
 # https://devimages.apple.com.edgekey.net/downloads/xcode/simulators/index-3905972D-B609-49CE-8D06-51ADC78E07BC.dvtdownloadableindex
 else
-    [ "$macOSversionMinor" -eq 7 ] && DMGURL=http://devimages.apple.com.edgekey.net/downloads/xcode/command_line_tools_for_xcode_os_x_lion_april_2013.dmg
-    [ "$macOSversionMinor" -eq 8 ] && DMGURL=http://devimages.apple.com.edgekey.net/downloads/xcode/command_line_tools_for_osx_mountain_lion_april_2014.dmg
+    [[ "$macOSversionMinor" -eq 7 ]] && DMGURL=http://devimages.apple.com.edgekey.net/downloads/xcode/command_line_tools_for_xcode_os_x_lion_april_2013.dmg
+    [[ "$macOSversionMinor" -eq 8 ]] && DMGURL=http://devimages.apple.com.edgekey.net/downloads/xcode/command_line_tools_for_osx_mountain_lion_april_2014.dmg
 
     TOOLS=clitools.dmg
     curl "$DMGURL" -o "$TOOLS"
     TMPMOUNT=`/usr/bin/mktemp -d /tmp/clitools.XXXX`
     hdiutil attach "$TOOLS" -mountpoint "$TMPMOUNT"
-    if [ "$macOSversionMinor" -eq 7 ]; then
+    if [[ "$macOSversionMinor" -eq 7 ]]; then
         # using '-allowUntrusted' because Lion CLI tools are so old Apple never built another
         # package that doesn't have an expired CA cert. (Expired February 15, 2015)
         installer -pkg "$(find $TMPMOUNT -name '*.mpkg')" -allowUntrusted -target /
